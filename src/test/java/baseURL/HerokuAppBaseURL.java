eb7785fde2a51fec38e08edd3297f407d5e5a90b
Yusuf Renkoglu <yusuf@renkoglu.com>
2023-02-03 23:32:31 +0200

    Api Testing v1

--- src/test/java/baseURL/HerokuAppBaseURL.java
+++ src/test/java/baseURL/HerokuAppBaseURL.java
@@ -4,7 +4,9 @@ import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.specification.RequestSpecification;
 
 public class HerokuAppBaseURL {
+
     public static RequestSpecification spec;
+
     static {
         spec = new RequestSpecBuilder().setBaseUri("https://restful-booker.herokuapp.com").build();
     }
